fa1e0d950f1b3b24eb67b1384f063d52afd58352
Alejandro Perdomo <aperdomobo@gmail.com>
2018-11-18 13:01:42 -0500

    add component session layer

--- src/main/java/syoux/wallets/component/DefaultSessionComponent.java
+++ src/main/java/syoux/wallets/component/DefaultSessionComponent.java
@@ -1,12 +1,22 @@
 package syoux.wallets.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import syoux.wallets.domain.SessionDomain;
 import syoux.wallets.model.UserSession;
+import syoux.wallets.service.DefaultSessionService;
 
 @Component
 public class DefaultSessionComponent implements SessionComponent {
+  private final SessionDomain sessionDomain;
+
+  @Autowired
+  public DefaultSessionComponent(final SessionDomain sessionDomain) {
+    this.sessionDomain = sessionDomain;
+  }
+
   @Override
   public void init(UserSession session) {
-
+    this.sessionDomain.init(session);
   }
 }
---
+++ src/main/java/syoux/wallets/domain/SessionDomain.java
@@ -0,0 +1,15 @@
+package syoux.wallets.domain;
+
+import org.springframework.stereotype.Component;
+import syoux.wallets.model.UserSession;
+
+@Component
+public class SessionDomain {
+  public SessionDomain() {
+
+  }
+
+  public void init(UserSession session) {
+
+  }
+}
